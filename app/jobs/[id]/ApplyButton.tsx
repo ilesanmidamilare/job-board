dc874cd10b556eda74b4e70e1717f2e17ac450a3
ilesanmidamilare <ilesanmi26@gmail.com>
2025-10-06 21:31:37 +0100

    added apply page

--- app/jobs/[id]/ApplyButton.tsx
+++ app/jobs/[id]/ApplyButton.tsx
@@ -1,5 +1,7 @@
 "use client"
+import { MoveRight } from 'lucide-react'
 import { useSession } from 'next-auth/react'
+import Link from 'next/link'
 import { useRouter } from 'next/router'
 import React, { useState } from 'react'
 
@@ -7,22 +9,29 @@ const ApplyButton = ({jobId}: {jobId: string}) => {
   const {data: session, status} = useSession()
   const router = useRouter()
   const [errorMessage, setErrorMessage] = useState<string>("")
-  const handleApply = () => {
+  const [applicationStatus, setApplicationStatus] = useState<"idle" | "success" | "error">("idle")
+  const handleApply = async() => {
     if(!session){
       router.push("/auth/signin")
       return 
     }
 
     setErrorMessage('')
+    setApplicationStatus('idle')
 
     try{
 
+      const response = await fetch(`/api/jobs/${jobId}/apply`, {
+        method: "POST"
+      })
+      setApplicationStatus("success")
     }catch(error){
       if(error instanceof Error){
           setErrorMessage(error.message)
       } else {
         setErrorMessage("Failed to apply for the job")
       }
+      setApplicationStatus("error")
         
     }
 
@@ -30,6 +39,15 @@ const ApplyButton = ({jobId}: {jobId: string}) => {
   if(status === "loading"){
     return <button disabled> Loading...</button>
   }
+
+  if(applicationStatus === "success") {
+    return (
+      <div>
+        <p>Application submited successfull</p>
+        <Link href={"/dashboard"}>View your application <MoveRight /></Link>
+      </div>)
+  }
+
   return (
     <>
         <button
@@ -37,6 +55,7 @@ const ApplyButton = ({jobId}: {jobId: string}) => {
         >
             Apply for this position
         </button>
+        {applicationStatus === 'error' && <p>{errorMessage}</p>}
     </>
   )
 }
